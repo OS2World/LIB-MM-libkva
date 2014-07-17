afb3e8a127709a13658d1834aa64220968119dc5
Martin Iturbide <martin@os2world.com>
2014-07-16 21:46:15 -0500

    version 1.2.1

---
+++ include/kva.h
@@ -0,0 +1,136 @@
+/*
+    K Video Accelerator library for OS/2
+    Copyright (C) 2007-2012 by KO Myung-Hun <komh@chollian.net>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __KVA_H__
+#define __KVA_H__
+
+#include <os2.h>
+#include <mmioos2.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define KVA_VERSION "1.2.1"
+
+#define KVAM_AUTO   0
+#define KVAM_DIVE   1
+#define KVAM_WO     2
+#define KVAM_SNAP   3
+#define KVAM_VMAN   4
+
+#define KVAR_NONE       0
+#define KVAR_ORIGINAL   1
+#define KVAR_FORCE43    2
+#define KVAR_FORCE169   3
+#define KVAR_FORCEANY   4
+
+#define KVAE_NO_ERROR                      0
+#define KVAE_NOT_INITIALIZED            ( -1 )
+#define KVAE_ALREADY_INITIALIZED        ( -2 )
+#define KVAE_INVALID_PARAMETER          ( -3 )
+#define KVAE_ALREADY_LOCKED             ( -4 )
+#define KVAE_NOT_LOCKED                 ( -5 )
+#define KVAE_CANNOT_SUBCLASS            ( -6 )
+#define KVAE_CANNOT_LOAD_WO             ( -7 )
+#define KVAE_NO_SUPPORTED_FOURCC        ( -8 )
+#define KVAE_WO_PALETTIZED_MODE         ( -9 )
+#define KVAE_NO_ATTRIBUTE               ( -10 )
+#define KVAE_CANNOT_LOAD_SNAP_WRAPPER   ( -11 )
+#define KVAE_CANNOT_LOAD_SNAP_DRIVER    ( -12 )
+#define KVAE_CANNOT_ALLOC_VIDEO_BUFFER  ( -13 )
+#define KVAE_CANNOT_SETUP               ( -14 )
+#define KVAE_HW_IN_USE                  ( -15 )
+#define KVAE_NOT_ENOUGH_MEMORY          ( -16 )
+#define KVAE_CANNOT_LOAD_DIVE           ( -17 )
+#define KVAE_CANNOT_LOAD_VMAN           ( -18 )
+#define KVAE_VMAN_ERROR                 ( -19 )
+
+#define KVAA_BRIGHTNESS 0
+#define KVAA_CONTRAST   1
+#define KVAA_SATURATION 2
+#define KVAA_HUE        3
+#define KVAA_GAMMA      4
+#define KVAA_LAST       ( KVAA_GAMMA + 1 )
+
+#define KVAF_YUY2       0x00000001
+#define KVAF_YV12       0x00000002
+#define KVAF_YVU9       0x00000004
+#define KVAF_BGR24      0x00010000
+#define KVAF_BGR16      0x00020000
+#define KVAF_BGR15      0x00040000
+#define KVAF_BGR32      0x00080000
+
+#ifndef FOURCC_YV12
+#define FOURCC_YV12     mmioFOURCC( 'Y', 'V', '1', '2' )
+#endif
+
+#ifndef FOURCC_YVU9
+#define FOURCC_YVU9     mmioFOURCC( 'Y', 'V', 'U', '9' )
+#endif
+
+#pragma pack( 1 )
+
+typedef struct tagKVASETUP
+{
+    ULONG       ulLength;         //size of structure in bytes
+    RECTL       rclSrcRect;       //displayed subrectangle, top-left is (0,0)
+    SIZEL       szlSrcSize;       //source image size
+    ULONG       ulRatio;          //aspect ratio
+    ULONG       ulAspectWidth;    //aspect width, only for KVAR_FORCEANY
+    ULONG       ulAspectHeight;   //aspect height, only for KVAR_FORCEANY
+    ULONG       fccSrcColor;      //image format
+    BOOL        fInvert;          //image invert, only for DIVE
+    BOOL        fDither;          //image dither, only for DIVE
+} KVASETUP, *PKVASETUP;
+
+typedef struct tagKVACAPS
+{
+    ULONG   ulMode;
+    ULONG   ulDepth;
+    ULONG   cxScreen;
+    ULONG   cyScreen;
+    FOURCC  fccScreen;
+    ULONG   ulInputFormatFlags;
+    ULONG   ulRMask;
+    ULONG   ulGMask;
+    ULONG   ulBMask;
+} KVACAPS, *PKVACAPS;
+
+#pragma pack()
+
+APIRET APIENTRY kvaInit( ULONG kvaMode, HWND hwnd, ULONG ulKeyColor );
+APIRET APIENTRY kvaDone( VOID );
+APIRET APIENTRY kvaLockBuffer( PPVOID ppBuffer, PULONG pulBPL );
+APIRET APIENTRY kvaUnlockBuffer( VOID );
+APIRET APIENTRY kvaSetup( PKVASETUP pkvas );
+APIRET APIENTRY kvaCaps( PKVACAPS pkvac );
+APIRET APIENTRY kvaClearRect( PRECTL prcl );
+APIRET APIENTRY kvaAdjustDstRect( PRECTL prclSrc, PRECTL prclDst );
+APIRET APIENTRY kvaQueryAttr( ULONG ulAttr, PULONG pulValue );
+APIRET APIENTRY kvaSetAttr( ULONG ulAttr, PULONG pulValue );
+APIRET APIENTRY kvaResetAttr( VOID );
+APIRET APIENTRY kvaDisableScreenSaver( VOID );
+APIRET APIENTRY kvaEnableScreenSaver( VOID );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/kva.c
@@ -0,0 +1,450 @@
+/*
+    K Video Accelerator library for OS/2
+    Copyright (C) 2007-2012 by KO Myung-Hun <komh@chollian.net>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#define INCL_WIN
+#define INCL_GPI
+#define INCL_DOS
+#include <os2.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "kva.h"
+#include "kva_internal.h"
+#include "kva_dive.h"
+#include "kva_wo.h"
+#include "kva_snap.h"
+#include "kva_vman.h"
+
+#define KVA_SHARED_MEM_NAME "\\SHAREMEM\\KVA\\HWINUSE"
+
+DECLARE_PFN( APIRET, APIENTRY, g_pfnDone, ( VOID ));
+DECLARE_PFN( APIRET, APIENTRY, g_pfnLockBuffer, ( PPVOID ppBuffer, PULONG pulBPL ));
+DECLARE_PFN( APIRET, APIENTRY, g_pfnUnlockBuffer, ( VOID ));
+DECLARE_PFN( APIRET, APIENTRY, g_pfnSetup, ( PKVASETUP pkvas ));
+DECLARE_PFN( APIRET, APIENTRY, g_pfnCaps, ( PKVACAPS pkvac ));
+DECLARE_PFN( APIRET, APIENTRY, g_pfnQueryAttr, ( ULONG ulAttr, PULONG pulValue ));
+DECLARE_PFN( APIRET, APIENTRY, g_pfnSetAttr, ( ULONG ulAttr, PULONG pulValue ));
+
+HWND  g_hwndKVA = NULLHANDLE;
+ULONG g_ulKeyColor = -1;
+
+static ULONG    m_ulRatio = KVAR_NONE;
+static ULONG    m_ulAspectWidth = -1;
+static ULONG    m_ulAspectHeight = -1;
+
+static BOOL     m_fKVAInited = FALSE;
+static BOOL     m_fLocked = FALSE;
+
+static PBOOL    m_pfHWInUse = NULL;
+static BOOL     m_fHWInUse = FALSE;
+
+static HMODULE  m_hmodSSCore = NULLHANDLE;
+static PFN      m_pfnSSCore_TempDisable = NULL;
+static PFN      m_pfnSSCore_TempEnable = NULL;
+
+APIRET APIENTRY kvaInit( ULONG kvaMode, HWND hwnd, ULONG ulKeyColor )
+{
+    ULONG   rc;
+
+    if( m_fKVAInited )
+        return KVAE_ALREADY_INITIALIZED;
+
+    m_fKVAInited = FALSE;
+    m_fLocked = FALSE;
+    m_fHWInUse = FALSE;
+
+    m_ulRatio = KVAR_NONE;
+    m_ulAspectWidth = -1;
+    m_ulAspectHeight = -1;
+
+    m_hmodSSCore = NULLHANDLE;
+    m_pfnSSCore_TempDisable = NULL;
+    m_pfnSSCore_TempEnable = NULL;
+
+    g_pfnDone = NULL;
+    g_pfnLockBuffer = NULL;
+    g_pfnUnlockBuffer = NULL;
+    g_pfnSetup = NULL;
+
+    g_hwndKVA = NULLHANDLE;
+    g_ulKeyColor = -1;
+
+    rc = KVAE_INVALID_PARAMETER;
+
+    if( !hwnd )
+        return rc;
+
+    if( DosGetNamedSharedMem( &m_pfHWInUse, KVA_SHARED_MEM_NAME, fPERM ) == 0 )
+    {
+        if( *m_pfHWInUse )
+        {
+            if( kvaMode == KVAM_SNAP || kvaMode == KVAM_WO )
+            {
+                DosFreeMem( m_pfHWInUse );
+
+                return KVAE_HW_IN_USE;
+            }
+            else if( kvaMode == KVAM_AUTO )
+                kvaMode = KVAM_DIVE;
+        }
+    }
+    else
+    {
+        if( DosAllocSharedMem( &m_pfHWInUse, KVA_SHARED_MEM_NAME, sizeof( BOOL ), fALLOC ) != 0 )
+            return KVAE_NOT_ENOUGH_MEMORY;
+
+        *m_pfHWInUse = FALSE;
+    }
+
+    g_hwndKVA = hwnd;
+    g_ulKeyColor = ulKeyColor;
+
+    if( kvaMode == KVAM_SNAP || kvaMode == KVAM_AUTO )
+    {
+        rc = snapInit();
+        if( rc )
+        {
+            if( kvaMode != KVAM_AUTO )
+                return rc;
+        }
+        else
+            kvaMode = KVAM_SNAP;
+    }
+
+    if( kvaMode == KVAM_WO || kvaMode == KVAM_AUTO )
+    {
+        rc = woInit();
+        if( rc )
+        {
+            if( kvaMode != KVAM_AUTO )
+                return rc;
+        }
+        else
+            kvaMode = KVAM_WO;
+    }
+
+    if( kvaMode == KVAM_VMAN || kvaMode == KVAM_AUTO )
+    {
+        rc = vmanInit();
+        if( rc )
+        {
+            if( kvaMode != KVAM_AUTO )
+                return rc;
+        }
+        else
+            kvaMode = KVAM_VMAN;
+    }
+
+    if( kvaMode == KVAM_DIVE || kvaMode == KVAM_AUTO )
+    {
+        rc = diveInit();
+        if( rc )
+        {
+            if( kvaMode != KVAM_AUTO )
+                return rc;
+        }
+        else
+            kvaMode = KVAM_DIVE;
+    }
+
+    if( !rc )
+    {
+        UCHAR szError[ 256 ];
+
+        if( kvaMode == KVAM_SNAP || kvaMode == KVAM_WO )
+            *m_pfHWInUse = m_fHWInUse = TRUE;
+
+        if( DosLoadModule( szError, sizeof( szError ), "SSCORE", &m_hmodSSCore ) == 0 )
+        {
+            DosQueryProcAddr( m_hmodSSCore, 0, "SSCore_TempDisable", &m_pfnSSCore_TempDisable );
+            DosQueryProcAddr( m_hmodSSCore, 0, "SSCore_TempEnable", &m_pfnSSCore_TempEnable );
+
+            if( !m_pfnSSCore_TempDisable || !m_pfnSSCore_TempEnable )
+            {
+                DosFreeModule( m_hmodSSCore );
+
+                m_hmodSSCore = NULLHANDLE;
+                m_pfnSSCore_TempDisable = NULL;
+                m_pfnSSCore_TempEnable = NULL;
+            }
+        }
+
+        m_fKVAInited = TRUE;
+    }
+
+    return rc;
+}
+
+APIRET APIENTRY kvaDone( VOID )
+{
+    ULONG rc;
+
+    if( !m_fKVAInited )
+        return KVAE_NOT_INITIALIZED;
+
+    rc = g_pfnDone();
+    if( rc )
+        return rc;
+
+    DosFreeModule( m_hmodSSCore );
+
+    if( m_fHWInUse )
+        *m_pfHWInUse = FALSE;
+
+    DosFreeMem( m_pfHWInUse );
+
+    m_fKVAInited = FALSE;
+
+    return KVAE_NO_ERROR;
+}
+
+APIRET APIENTRY kvaLockBuffer( PPVOID ppBuffer, PULONG pulBPL )
+{
+    ULONG   rc;
+
+    if( !m_fKVAInited )
+        return KVAE_NOT_INITIALIZED;
+
+    if( m_fLocked )
+        return KVAE_ALREADY_LOCKED;
+
+    if( ppBuffer == NULL || pulBPL == NULL )
+        return KVAE_INVALID_PARAMETER;
+
+    rc = g_pfnLockBuffer( ppBuffer, pulBPL );
+    if( rc )
+        return rc;
+
+    m_fLocked = TRUE;
+
+    return KVAE_NO_ERROR;
+}
+
+APIRET APIENTRY kvaUnlockBuffer( VOID )
+{
+    ULONG   rc;
+
+    if( !m_fKVAInited )
+        return KVAE_NOT_INITIALIZED;
+
+    if( !m_fLocked )
+        return KVAE_NOT_LOCKED;
+
+    rc = g_pfnUnlockBuffer();
+    if( rc )
+        return rc;
+
+    m_fLocked = FALSE;
+
+    return 0;
+}
+
+APIRET APIENTRY kvaSetup( PKVASETUP pkvas )
+{
+    ULONG rc;
+
+    if( !m_fKVAInited )
+        return KVAE_NOT_INITIALIZED;
+
+    if( !pkvas )
+        return KVAE_INVALID_PARAMETER;
+
+    m_ulRatio = pkvas->ulRatio;
+    m_ulAspectWidth = pkvas->ulAspectWidth;
+    m_ulAspectHeight = pkvas->ulAspectHeight;
+
+    rc = g_pfnSetup( pkvas );
+
+    kvaClearRect( NULL );
+
+    return rc;
+}
+
+APIRET APIENTRY kvaCaps( PKVACAPS pkvac )
+{
+    if( !m_fKVAInited )
+        return KVAE_NOT_INITIALIZED;
+
+    if( !pkvac )
+        return KVAE_INVALID_PARAMETER;
+
+    memset( pkvac, 0, sizeof( PKVACAPS ));
+
+    return g_pfnCaps( pkvac );
+}
+
+APIRET APIENTRY kvaClearRect( PRECTL prcl )
+{
+    HPS     hps;
+    RECTL   rcl;
+
+    if( !m_fKVAInited )
+        return KVAE_NOT_INITIALIZED;
+
+    hps = WinGetPS( g_hwndKVA );
+
+    if( !prcl )
+    {
+        prcl = &rcl;
+        WinQueryWindowRect( g_hwndKVA, prcl );
+    }
+    GpiCreateLogColorTable( hps, 0, LCOLF_RGB, 0, 0, NULL );
+    WinFillRect( hps, prcl, g_ulKeyColor );
+
+    WinReleasePS( hps );
+
+    return KVAE_NO_ERROR;
+}
+
+APIRET APIENTRY kvaAdjustDstRect( PRECTL prclSrc, PRECTL prclDst )
+{
+    ULONG   cxSrc, cySrc;
+    ULONG   cxDst, cyDst;
+    ULONG   cxDstNew, cyDstNew;
+
+    if( !m_fKVAInited )
+        return KVAE_NOT_INITIALIZED;
+
+    if(( !prclSrc && m_ulRatio == KVAR_ORIGINAL ) || !prclDst )
+        return KVAE_INVALID_PARAMETER;
+
+    WinQueryWindowRect( g_hwndKVA, prclDst );
+
+    cxDst = prclDst->xRight;
+    cyDst = prclDst->yTop;
+
+    switch( m_ulRatio )
+    {
+
+        case KVAR_ORIGINAL :
+        case KVAR_FORCE43  :
+        case KVAR_FORCE169 :
+        case KVAR_FORCEANY :
+            switch( m_ulRatio )
+            {
+                case KVAR_ORIGINAL :
+                    cxSrc = labs( prclSrc->xRight - prclSrc->xLeft );
+                    cySrc = labs( prclSrc->yTop - prclSrc->yBottom );
+                    break;
+
+                case KVAR_FORCE43 :
+                    cxSrc = 4;
+                    cySrc = 3;
+                    break;
+
+                case KVAR_FORCE169 :
+                    cxSrc = 16;
+                    cySrc = 9;
+                    break;
+
+                case KVAR_FORCEANY :
+                default :               // to prevent from uninitializing warning
+                    cxSrc = m_ulAspectWidth;
+                    cySrc = m_ulAspectHeight;
+                    break;
+            }
+
+            cxDstNew = cxDst;
+            cyDstNew = cxDstNew * cySrc / cxSrc;
+            if( cyDstNew > cyDst )
+            {
+                cyDstNew = cyDst;
+                cxDstNew = cyDstNew * cxSrc / cySrc;
+            }
+            break;
+
+        case KVAR_NONE :
+        default :   // assume KVAR_NONE
+            cxDstNew = cxDst;
+            cyDstNew = cyDst;
+            break;
+    }
+
+    prclDst->xRight = cxDstNew;
+    prclDst->yTop = cyDstNew;
+
+    WinOffsetRect( WinQueryAnchorBlock( g_hwndKVA ), prclDst,
+                   ( cxDst - cxDstNew ) / 2, ( cyDst - cyDstNew ) / 2 );
+
+    return KVAE_NO_ERROR;
+}
+
+APIRET APIENTRY kvaQueryAttr( ULONG ulAttr, PULONG pulValue )
+{
+    if( !m_fKVAInited )
+        return KVAE_NOT_INITIALIZED;
+
+    if( !pulValue )
+        return KVAE_INVALID_PARAMETER;
+
+    return g_pfnQueryAttr( ulAttr, pulValue );
+}
+
+APIRET APIENTRY kvaSetAttr( ULONG ulAttr, PULONG pulValue )
+{
+    if( !m_fKVAInited )
+        return KVAE_NOT_INITIALIZED;
+
+    if( !pulValue )
+        return KVAE_INVALID_PARAMETER;
+
+    return g_pfnSetAttr( ulAttr, pulValue );
+}
+
+APIRET APIENTRY kvaResetAttr( VOID )
+{
+    ULONG ulValue;
+    int   i;
+
+    if( !m_fKVAInited )
+        return KVAE_NOT_INITIALIZED;
+
+    for( i = 0; i < KVAA_LAST; i++ )
+    {
+        ulValue = -1;
+        kvaSetAttr( i, &ulValue );
+    }
+
+    return KVAE_NO_ERROR;
+}
+
+APIRET APIENTRY kvaDisableScreenSaver( VOID )
+{
+    if( !m_fKVAInited )
+        return KVAE_NOT_INITIALIZED;
+
+    if( m_pfnSSCore_TempDisable )
+        m_pfnSSCore_TempDisable();
+
+    return KVAE_NO_ERROR;
+}
+
+APIRET APIENTRY kvaEnableScreenSaver( VOID )
+{
+    if( !m_fKVAInited )
+        return KVAE_NOT_INITIALIZED;
+
+    if( m_pfnSSCore_TempEnable )
+        m_pfnSSCore_TempEnable();
+
+    return KVAE_NO_ERROR;
+}
+
+
